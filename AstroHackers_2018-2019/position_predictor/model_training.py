e2a4603623448419a6f4920a6e8587ebd0453d0c
apple <apple@appledeMacBook-Pro.local>
2019-06-15 18:06:56 +0200

    contest ended

--- AstroHackers_2018-2019/position_predictor/model_training.py
+++ AstroHackers_2018-2019/position_predictor/model_training.py
@@ -43,15 +43,43 @@ Y_pred =tf.add(tf.multiply(tf.math.sin(tf.multiply(tf.subtract(X,c),b)),a),d)
 #Y_pred = tf.multiply(tf.math.sin(tf.add(tf.multiply(b,X),c)),a)
 
 
-##initializing model
+##configure model
 # Mean Squared Error Cost Function
-cost = tf.reduce_sum(tf.pow(Y_pred-Y, 2)) / (2 * m)
+cost = loss = tf.reduce_sum(tf.pow(Y_pred - Y,2))/(2*m)
 
 # %% Use gradient descent to optimize W,b
 # Performs a single step in the negative gradient
-learning_rate = 0.001 #the learning rate of the optimizer
-optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+learning_rate = tf.placeholder(tf.float32, shape=None, name='learning_rate') #the learning rate of the optimizer
+optimizer = tf.train.AdamOptimizer(learning_rate)
+train_step = optimizer.minimize(cost)
+grads_and_vars = optimizer.compute_gradients(cost) #compute the gradient for the sake of debug
 
+###debuge part
+# Name scope allows you to group various summaries together
+# Summaries having the same name_scope will be displayed on the same row
+with tf.name_scope('performance'):
+    # Summaries need to be displayed
+    # Whenever you need to record the loss, feed the mean loss to this placeholder
+    tf_loss_ph = tf.placeholder(tf.float32,shape=None,name='loss_summary')
+    # Create a scalar summary object for the loss so it can be displayed
+    tf_loss_summary = tf.summary.scalar('loss', tf_loss_ph)
+
+    # Whenever you need to record the loss, feed the mean test accuracy to this placeholder
+    tf_accuracy_ph = tf.placeholder(tf.float32,shape=None, name='accuracy_summary')
+    # Create a scalar summary object for the accuracy so it can be displayed
+    tf_accuracy_summary = tf.summary.scalar('accuracy', tf_accuracy_ph)
+
+# Gradient norm summary
+for g,v in grads_and_vars:
+    with tf.name_scope('gradients'):
+        tf_last_grad_norm = tf.sqrt(tf.reduce_mean(g**2))
+        tf_gradnorm_summary = tf.summary.scalar('grad_norm', tf_last_grad_norm)
+        break
+
+# Merge all summaries together
+performance_summaries = tf.summary.merge([tf_loss_summary,tf_accuracy_summary])
+
+###train
 # Here we tell tensorflow that we want to initialize all
 # the variables in the graph so we can use them
 init = tf.global_variables_initializer()
@@ -63,15 +91,8 @@ saver = tf.train.Saver()
 sess = tf.Session()
 sess.run(init)
 
-##visualization of the computing graph(for debugging and clearity)
-#tensorboard is activated through:
-"""#tensorboard --logdir=./graphs"""
-"""writer = tf.summary.FileWriter('./graphs',sess.graph)
-tensorboard --logdir= ./graphs/logdir --debugger_port 7000
-"""
-
 ##execute the graph and train (uses mini-batch, which means it trains a sample[100] of th entire data in one iteration and eventually will cover every sub-sample)
-epoch = 100000 #number of itertions of corrections of the 'weights'(a,b,c)
+epoch = 10000 #number of itertions of corrections of the 'weights'(a,b,c)
 batch_size = 9677 #sample size
 n_batches = int(np.ceil(m / batch_size)) #number of samples
 co_list = list() #define cost array that keeps track of how the error rate changes[later used to be plotted]
@@ -87,13 +108,13 @@ def fetch_batch(epoch, batch_index, batch_size):
     y_batch = y2[indices-2] #the result shows that the batch doesn't match up, and the index are 2 different from each other, therefore the group decided just minus two here to fix the error
     return X_batch, y_batch
 
-#saver.restore(sess,"models/theta_predictor.ckpt") #restore variables
+saver.restore(sess,"models/theta_predictor.ckpt") #restore variables
 
 for step in range(epoch):
     # Feeding each a batch into the optimizer using Feed Dictionary
     for batch_index in range(n_batches):# for each batch index
         X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)#get the mini-batch used to train
-        sess.run(optimizer, feed_dict={X: X_batch, Y: y_batch}) #run gradient descent to correct weights
+        sess.run(train_step, feed_dict={X: X_batch, Y: y_batch, learning_rate: 0.01}) #run gradient descent to correct weights
 
     #calulate the step cost and add it into an array that records the progress of training
     step_co = float(sess.run(cost, feed_dict = {X : x, Y : y2}))
@@ -107,7 +128,8 @@ for step in range(epoch):
 
 print("final cost after this session of training: {}".format(sess.run(cost, feed_dict = {X : x, Y : y2})))
 co_list = np.round(np.array(co_list),2) # round the cost to two decimal places
-print(sess.run(Y_pred,feed_dict={X:x}))
+
+#print(sess.run(Y_pred,feed_dict={X:x}))
 """#compare predictions Vs y_true
 print(sess.run(Y_pred,feed_dict={X:x}))
 print(y2)"""
@@ -119,6 +141,13 @@ plt.ylabel('training cost')
 plt.xlabel('epochs')
 plt.show()
 ##print(sess.run(Y_pred, feed_dict = {X : x, Y : y2}))
+
+##visualization of the computing graph(for debugging and clearity)
+#tensorboard is activated through:
+"""#tensorboard --logdir=./graphs"""
+writer = tf.summary.FileWriter('./tensorboard_debug',sess.graph)
+#tensorboard --logdir= ./graphs --debugger_port 7000
+
 
 ###save path
 ##save_path = saver.save(sess, "model/r_predictor.ckpt")#save the model(that predicts r)
